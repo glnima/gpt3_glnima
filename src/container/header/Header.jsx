22a5c53dcf373f71292798cb0b4491d646bcd4d4
Nima Golestani <nima.golestani73@gmail.com>
2024-09-07 21:55:54 +0330

    Update Header.jsx
    Change Title

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className='gpt3__header section__padding' id="home">
       <div className='gpt3__header-content'>
         <h1 className='gradient__text'>
-          Let's Build Something Amazing With GPT-3 OpenAI
+          Let's Build Something Amazing With GPT Template
         </h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
         <div className='gpt3__header-content__input'>
